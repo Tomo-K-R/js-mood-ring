1231d18dbb18ab88bcdd1cd9ff2b89e606b4b722
Tomo-K-R <tomomikojo@hotmail.com>
2022-04-09 23:17:45 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var body = document.querySelector("body");
+var moodElement = document.querySelector("#moods");
+// drop-down list
+
+moodElement.addEventListener("change", function (e) {
+  var mood = e.target.value;
+
+  if (mood === "happy") {
+    body.classList.add("happy");
+    body.classList.remove("sad");
+    body.classList.remove("passionate");
+  } else if (mood === "sad") {
+    body.classList.add("sad");
+    body.classList.remove("happy");
+    body.classList.remove("passionate");
+  } else if (mood === "passionate") {
+    body.classList.add("passionate");
+    body.classList.remove("happy");
+    body.classList.remove("Sad");
+  }
+});
